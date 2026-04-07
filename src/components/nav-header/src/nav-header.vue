d858766cf1f5396b6af2fd48ce125e45fe610260
Jesen0823 <xie_stacol@163.com>
2026-04-07 18:07:53 +0800

    feat(nav-header pre): nav-header icon navigate with main

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -1,15 +1,59 @@
 <template>
-  <div></div>
+  <div class="nav-header">
+    <!--用法1：-->
+    <!--
+    <el-icon @click="handleFoldChange">
+      <template v-if="isFold">
+        <Fold />
+      </template>
+      <template else>
+        <Expand />
+      </template>
+    </el-icon>
+    -->
+    <!--用法2：-->
+    <el-icon class="fold-menu" @click="handleFoldChange">
+      <component :is="isFold ? 'Fold' : 'Expand'" />
+    </el-icon>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, Ref } from 'vue'
 
 export default defineComponent({
-  setup() {
-    return {}
+  emits: ['foldChange'],
+  setup(props, { emit }) {
+    const isFold = ref(false) as any
+    const handleFoldChange = () => {
+      isFold.value = !isFold.value
+      emit('foldChange', isFold.value)
+    }
+
+    return {
+      isFold,
+      handleFoldChange
+    }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.nav-header {
+  display: flex;
+  width: 100%;
+
+  .fold-menu {
+    font-size: 30px;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
+  }
+}
+</style>
